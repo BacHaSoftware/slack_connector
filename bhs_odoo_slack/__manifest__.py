032f8688c3b1bf04b69c584583471f5dbffd3bcd
Nguyen.Thi.Phuong.Dung <dungntp.bhsoft@gmail.com>
2025-02-21 15:06:23 +0700

    v17: update ava and banner

--- bhs_odoo_slack/__manifest__.py
+++ bhs_odoo_slack/__manifest__.py
@@ -18,7 +18,7 @@
     'external_dependencies': {
         'python': ['slackclient','html-slacker'],
     },
-    'images': ['static/description/banner.gif'],
+    'images': ['static/description/banner.png'],
     'installable': True,
     'application': True,
     'license': 'LGPL-3'
